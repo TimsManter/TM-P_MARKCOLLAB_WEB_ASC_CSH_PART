37335d768c32f408d0a19d08dc8d239d8fb1a4cc
TimsManter <timsmanter@gmail.com>
2017-06-28 14:09:07 +0200

    Create first version of DocumentsController

---
+++ Controllers/DocumentsController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MarkCollab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarkCollab.Controllers {
+  [Route("api/docs")]
+  public class DocumentsController : Controller {
+    private MarkCollabDbContext _context;
+    public DocumentsController(MarkCollabDbContext context) {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Document>> GetAllAsync() {
+      return await _context.Documents.ToListAsync();
+    }
+  }
+}
