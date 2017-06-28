b0b567a6866ed4111b5862bc49392998aab5a94a
TimsManter <timsmanter@gmail.com>
2017-06-28 13:07:43 +0200

    Add roles DbSet

--- Models/MarkCollabDbContext.cs
+++ Models/MarkCollabDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace MarkCollab.Models {
   public class MarkCollabDbContext : DbContext {
     public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
     public DbSet<Document> Documents { get; set; }
     public DbSet<Token> Tokens { get; set; }
 
