fb7ef1740e6224f7148fcafc7606d56b79e21fbc
TimsManter <timsmanter@gmail.com>
2017-06-29 08:44:49 +0200

    Remove overrided key in model

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace MarkCollab.Models
 {
-  public class User : IdentityUser
+  public class User : IdentityUser<int>
   {
-    public string Name { get; set; }
     public List<Document> Documents { get; set; }
   }
 }
